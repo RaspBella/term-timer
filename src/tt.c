6f90d289a4cecf95341891710f1324d2a449527b
Bella Campbell <Bella.Campbell@tuta.io>
2023-10-03 21:14:38 +0100

    Added windows support and an error for unsupported OS's

--- src/tt.c
+++ src/tt.c
@@ -30,6 +30,13 @@ int main(void)
     filename = realloc(filename, strlen(filename)+11);
     filename = strcat(filename, "/.cache/tt");
     filename[strlen(filename)] = 0;
+    #elif _WIN32
+    filename = strdup(getenv("USERPROFILE"));
+    filename = realloc(filename, strlen(filename)+12);
+    filename = strcat(filename, "\\AppData\\tt");
+    filename[strlen(filename)] = 0;
+    #else
+    #error Your OS is not supported
     #endif
 
     fp = fopen(filename, "rb");
